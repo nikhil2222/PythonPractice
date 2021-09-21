2d424b0280d2af772c60914ca02dba71ed65e9db
navnit0707 <knavnit81@gmail.com>
2021-09-21 20:10:51 +0530

    completed first program in python

---
+++ question1.py
@@ -0,0 +1,25 @@
+""""
+1. TypeCast And Double It
+Given an input num as a string. 
+You need to typecast into an integer and double it.
+
+"""
+
+def utility():
+    #The line below takes the input
+    num=input()
+    
+    #Complete the syntax below. Convert num to int and double it
+    ans=int(num)
+    #Complete the syntax above
+    
+    #The line below prints the output
+    print(2*ans)
+
+def main():
+    t = int(input())
+    while(t>0):
+        utility()
+        t-=1
+if __name__ == "__main__":
+    main()
